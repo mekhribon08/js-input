6527e8a092c5dad2b1052cba0feb87912f6fdfe1
mekhribon08 <muxayyoxoljigitova79@gmail.com>
2022-08-11 11:49:58 +0400

    js-praktic

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var dollorToSum = 10966.96;
+var rublToSum = 181.868;
+var euroToSum = 11181.36;
+
+var elInput = document.querySelector(".js-input");
+var elUsd = document.querySelector(".js-usd");
+var elRubl = document.querySelector("js-rubl");
+var elEuro = document.querySelector(".js-euro");
+var elForm = document.querySelector(".js-form");
+var elText = document.querySelector(".js-text");
+
+// elForm.addEventListener("submit", function (evt) {
+//   evt.preventDefault();
+
+//   elText.textContent = elInput.value;
+// });
